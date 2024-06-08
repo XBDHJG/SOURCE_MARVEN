26dbb222c1cacaa71dbbe033d5809669d41813d0
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-06-08 21:32:23 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["XB_DV"]
+OWNER_NAME = "devail"
+BOT_TOKEN = "7425080038:AAF1nEgPJq39yUeICySir7T6FM_UqeodCfg"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/zombieyy"
+GROUP = "https://t.me/hvfffgyuuiiijj"
+VIDEO = "https://telegra.ph/file/770e2d5df0b50264097b2.jpg"
+LOGS = "XB_DV"
+
