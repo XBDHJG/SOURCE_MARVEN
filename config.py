6c490839d22a0de3856516b08a6b78233ced1bcd
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-06-08 22:36:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,42 @@
 import re
 from os import getenv
-import os
-from os import getenv
-from dotenv import load_dotenv
-from OWNER import BOT_TOKEN, OWNER, OWNER_NAME, DATABASE, CHANNEL, GROUP, LOGS, VIDEO
+
 from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
 
+import os
+from os import getenv
+from dotenv import load_dotenv
+from OWNER import BOT_TOKEN, OWNER, OWNER_NAME, DATABASE, CHANNEL, GROUP, LOGS, VIDEO
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+admins = {}
+user = {}
+call = {}
+dev = {}
+logger = {}
+logger_mode = {}
+botname = {}
+appp = {}
+helper = {}
+
+
+
+API_ID = int(getenv("API_ID", "8186557"))
+API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
+BOT_TOKEN = BOT_TOKEN
+MONGO_DB_URL = DATABASE
+OWNER = OWNER
+OWNER_NAME = OWNER_NAME
+CHANNEL = CHANNEL
+GROUP = GROUP
+LOGS = LOGS
+VIDEO = VIDEO
 # Get this value from my.telegram.org/apps
 API_ID = int(getenv("API_ID", "10823881"))
 API_HASH = getenv("API_HASH", '339886e2109eb67203ce12022b32e035')
