2f5e337692df673729d58d41c148b5d3659a67cb
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-06-09 08:48:01 +0300

    Update bot.py

--- MarvenMusic/core/bot.py
+++ MarvenMusic/core/bot.py
@@ -1,45 +1,21 @@
-from pyrogram import Client, errors
-from pyrogram.enums import ChatMemberStatus, ParseMode
-
-import config
-
-from ..logging import LOGGER
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+from pyromod import listen
 
 
-class Marven(Client):
-    def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
-        super().__init__(
-            name="MarvenMusic",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
-        )
 
-    async def start(self):
-        await super().start()
-        self.id = self.me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.username = self.me.username
-        self.mention = self.me.mention
+bot = Client(
+    "mo",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="Maker")
+    )
 
-        try:
-            await self.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"ꨄ <u><b>» {self.mention} تم تشغيل البوت بنجاح . :</b><u>\n\nꨄ 𝒊𝑫 : <code>{self.id}</code>\nꨄ 𝑵𝒆𝒎𝒆 : {self.name}\nꨄ 𝑼𝒔𝒆𝒓𝑵𝒆𝒎𝒆 : @{self.username}",
-            )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
-            )
-            exit()
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
-            )
-            exit()
-
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
-
-    async def stop(self):
-        await super().stop()
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
+    await bot.start()
+    devail = "XB_DV"
+    await bot.send_message(zombie, "**تم تشغيل ال صانع عزيزي المطور ،**")
+    print("[INFO]: تم تشغيل الصانع وارسال رسالة للمطور💎.")
+    await idle()
