10d2de6307cd4321e7f2a1e34b691a8cab57bd14
XBDHJG <129221851+XBDHJG@users.noreply.github.com>
2024-06-09 16:12:40 +0300

    Update start.py

--- MarvenMusic/utils/inline/start.py
+++ MarvenMusic/utils/inline/start.py
@@ -28,6 +28,23 @@ def changeImageSize(maxWidth, maxHeight, image):
     newHeight = int(heightRatio * image.size[1])
     newImage = image.resize((newWidth, newHeight))
     return newImage
+def private_panel(_):
+    buttons = [
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+        ],[
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+    ]
+    return buttons
 
 
 async def gen_bot(client, username, photo):
